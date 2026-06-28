3980ecbd4882e05e16bc78de56b13ced181e345e
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-28 23:49:15 +0300

    add colors + header

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,59 @@
+import { RiGlobalLine, RiMenuLine } from "@remixicon/react";
+import "./header.scss";
+import Logo from "./Logo";
+
+// 1. تصحيح الكلمات الإملائية في الواجهة
+interface HeaderIconItem {
+  type: "menu" | "list";
+  labels?: string[];
+  icons?: React.ReactNode[];
+}
+
+// 2. تعديل البيانات وتصحيح التسميات
+const listHeaderIcons: HeaderIconItem[] = [
+  {
+    type: "list",
+    labels: ["Homes", "Experiences", "Services"],
+  },
+  {
+    type: "menu",
+    icons: [<RiGlobalLine />, <RiMenuLine />],
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header_container">
+        <div className="logo">
+          <Logo />
+        </div>
+
+        {listHeaderIcons.map((item) => (
+          <div key={item.type} className={item.type}>
+            {item.type === "list" && (
+              <>
+                {item.labels && (
+                  <ul>
+                    {item.labels.map((label, ind) => (
+                      <li key={ind}>{label}</li>
+                    ))}
+                  </ul>
+                )}
+              </>
+            )}
+
+            {item.type === "menu" && (
+              <>
+                {item.icons &&
+                  item.icons.map((icon, i) => <div key={i}>{icon}</div>)}
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/layout/Header/Logo.tsx
@@ -0,0 +1,35 @@
+
+
+interface Props {
+  width?: number | string;
+  height?: number | string;
+}
+
+const Logo = ({ width = 50, height = 60 }: Props) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ display: "inline-block", verticalAlign: "middle" }}>
+      <path
+        d="M 50 62.5 
+       C 43.5 54, 43.5 45.5, 50 45.5 
+       C 56.5 45.5, 56.5 54, 50 62.5 
+       C 40 76.5, 23 79, 17.5 73.5 
+       C 11 67, 28 35, 50 14.5 
+       C 72 35, 89 67, 82.5 73.5 
+       C 77 79, 60 76.5, 50 62.5 Z"
+        fill="none"
+        stroke="#FF5A5F"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
