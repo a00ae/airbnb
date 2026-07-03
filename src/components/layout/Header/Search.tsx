65fe049dd8b7b1be57954a523fedcb95d8d63fb1
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-07-04 00:02:53 +0300

    new hook

--- src/components/layout/Header/Search.tsx
+++ src/components/layout/Header/Search.tsx
@@ -1,57 +1,50 @@
 import { RiSearchLine } from "@remixicon/react";
 import { itemButtonSearch } from "./index";
 import "./search.scss";
-import { useEffect, useRef, useState } from "react";
-
-
+import { useRef, useState } from "react";
+import { useOnclickOutSide } from "../../../hook/useOnclickOutSide";
 
 const Search = () => {
   const [activeLabel, setActiveLabel] = useState<string | null>(null);
 
-    const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
 
-    const handleDocumentClick = (e: MouseEvent) => {
-      if (ref.current && !ref.current.contains(e.target as Node)) {
-        setActiveLabel(null);
-      }
-    };
+  const handleClicOutSide = () => {
+    setActiveLabel(null);
+  };
 
-    useEffect(() => {
-      if (activeLabel) window.addEventListener("click", handleDocumentClick);
-      return () => {
-        window.removeEventListener("click", handleDocumentClick);
-      };
-    }, [activeLabel]);
-  
+  useOnclickOutSide({
+    ref: ref,
+    handleDocumentClick: handleClicOutSide,
+    visible: activeLabel,
+  });
 
   const handleClickSearch = (label: string) => {
     setActiveLabel((prev) => (prev === label ? null : label));
   };
-  if (!ref) return null;
 
   return (
     <div ref={ref} className="search">
       {itemButtonSearch.map(({ descraption, title }) => {
-        const currentTitleLower:string = title.trim().toLowerCase();
-        const isCurrentActive:boolean = activeLabel === title;
+        const currentTitleLower: string = title.trim().toLowerCase();
+        const isCurrentActive: boolean = activeLabel === title;
 
         return (
           <div
             onClick={() => handleClickSearch(title)}
             key={title}
-            className={`search_btn ${currentTitleLower}`}
-          >
+            className={`search_btn ${currentTitleLower}`}>
             <p>{title}</p>
             <span>{descraption}</span>
-            
+
             {title === "Who" && (
               <div className="search_icon">
                 <RiSearchLine />
               </div>
             )}
 
-            <div className={`last-${currentTitleLower} ${isCurrentActive ? "visible" : ""}`}>
-            </div>
+            <div
+              className={`last-${currentTitleLower} ${isCurrentActive ? "visible" : ""}`}></div>
           </div>
         );
       })}
--- src/hook/useOnclickOutSide.tsx
+++ src/hook/useOnclickOutSide.tsx
@@ -1,15 +1,27 @@
 import type React from "react"
 import { useEffect } from "react";
 
-
 type OutSide = {
-    ref: React.RefObject<HTMLDivElement | null>
-    handleClickOutSide: () => void;
+  ref: React.RefObject<HTMLDivElement | null>;
+  handleDocumentClick: () => void;
+  visible?: string | boolean | null;
 }
 
-export const onClickOutSide = ({ref, handleClickOutSide}: OutSide) => {
-    
+export const useOnclickOutSide = ({ ref, handleDocumentClick, visible }: OutSide) => {
+  
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handleDocumentClick();
+      }
+    };
 
+    if (visible) {
+      window.addEventListener("click", handleClick);
+    }
 
-
-}
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, [visible, ref, handleDocumentClick]); // إضافة جميع التبعيات هنا
+};
